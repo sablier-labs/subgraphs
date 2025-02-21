dc7955601c8dd9ab627b0429f6d9e22ae43560f2
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2025-02-21 18:31:37 +0200

    chore: add form addresses, deploy subgraphs

--- packages/constants/src/addresses/form.ts
+++ packages/constants/src/addresses/form.ts
@@ -1,8 +1,8 @@
 export let chainId = 478;
 export let chain = "form";
-export let startBlock_lockup = 3357800;
-export let startBlock_merkle = 3357800;
-export let startBlock_flow = 3357800;
+export let startBlock_lockup = 3359280;
+export let startBlock_merkle = 3363710;
+export let startBlock_flow = 3363700;
 
 /** Rule: keep addresses lowercased */
 
@@ -17,11 +17,17 @@ export let dynamic: string[][] = [];
 
 export let tranched: string[][] = [];
 
-export let merged: string[][] = [["", "LK", "V23"]];
+export let merged: string[][] = [
+  ["0xa2dd5e785aa0225d681416884d395c7e22d92850", "LK", "V23"],
+];
 
-export let flow: string[][] = [["", "FL2", "V11"]];
+export let flow: string[][] = [
+  ["0x5dd399bb320412df92df5c10484d3f8d481fe231", "FL2", "V11"],
+];
 
-export let factory: string[][] = [["", "MSF4", "V23"]];
+export let factory: string[][] = [
+  ["0xa9264ef7cb1516cc27fcd5149a2909ace885ffb6", "MSF4", "V23"],
+];
 
 /** PRBProxy registry */
 export let registry = "";
