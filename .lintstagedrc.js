8ec320ac333cae42067def7df53a336871cf1b9a
Paul Razvan Berg <prberg@proton.me>
2025-06-24 18:45:08 +0300

    chore: update comments and search exclude
    chore: fix lintstaged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,9 +5,9 @@ module.exports = {
   "*.{graphql,js,json,jsonc,ts}": "pnpm biome check --write",
   "*.{js,ts}": "pnpm biome lint --write --only correctness/noUnusedImports",
   "*.{md,yaml,yml}": "pnpm prettier --cache --write",
-  "src/exports/schemas/*.graphql": (stagedFiles) => {
+  "src/exports/schemas/*.graphql": (_stagedFiles) => {
     const codegenSchema = "just codegen-schema envio all";
-    const exportSchemas = `just export-schemas ${stagedFiles.join(" ")}`;
+    const exportSchemas = "just export-schemas";
     return [codegenSchema, exportSchemas];
   },
 };
